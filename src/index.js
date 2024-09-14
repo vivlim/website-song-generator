ae8ad0784bf79fa157706810bec935d4bcd83583
Viv Lim <git@vvn.space>
2024-09-14 01:00:59 -0700

    audible

--- src/index.js
+++ src/index.js
@@ -8,116 +8,52 @@ var compostInputData = window.performance.getEntries().map(function(e) {
 var lastInput = compostInputData[compostInputData.length-1]
 
 var audioContext = new AudioContext();
-window.compostState = {
+var compostState = {
     current: 0,
     abort: false,
-    loopAfter: lastInput.start + lastInput.size,
+    loopAfter: 4000,
     audioContext: audioContext,
     oscList:  [],
     mainGainNode:  audioContext.createGain(),
     contentTypeOscs: new Map(),
-    scale: Scale.get("c5 pentatonic"),
-    scaleDegrees: Scale.degrees("c5 pentatonic"),
+    scale: Scale.get("c3 pentatonic"),
+    scaleDegrees: Scale.degrees("c4 pentatonic"),
     inputData: compostInputData,
 }
-window.compostState.mainGainNode.connect(audioContext.destination);
-window.compostState.mainGainNode.gain.value = 0.5;
-
-// create one osc for each mime type
-for (const d of compostInputData){
-    if (window.compostState.contentTypeOscs.get(d.t) === undefined){
-        var osc = audioContext.createOscillator();
-        window.compostState.contentTypeOscs.set(d.t, osc);
-        osc.connect(compostState.mainGainNode);
-        osc.type = "sine";
-    }
-}
-
+window.compostState = compostState;
+compostState.mainGainNode.connect(audioContext.destination);
+compostState.mainGainNode.gain.value = 0.3;
 
 var stepSize = 10
 
 
-function playTone(freq) {
-  const osc = audioContext.createOscillator();
-  osc.connect(mainGainNode);
-  osc.type = "sine";
-
-  osc.frequency.value = freq;
-  osc.start();
-
-  return osc;
-}
-
-function stopAll(){
-    window.compostState.abort = true;
-    for (const o of window.compostState.contentTypeOscs.values()){
-        try {
-            o.stop();
-        }
-        catch {}
-    }
+function startPlayingData(d){
+    console.log("playing data " + d.start);
+    var osc = audioContext.createOscillator();
+    osc.connect(compostState.mainGainNode);
+    osc.type = "square";
+    var degree = d.size % 8 - 4;
+    var note = window.compostState.scaleDegrees(degree);
+    var freq = Note.freq(note);
+    console.log("mapped size " + d.size + " to degree " + degree + " to note " + note + " to freq " + freq);
+    osc.frequency.value = freq;
+    osc.start();
+    var duration = d.duration % compostState.loopAfter;
+    setTimeout(() => {stopPlayingData(d, osc)}, duration);
 }
 
 function stopPlayingData(d, osc){
-    console.log("stop started osc");
+    console.log("stopping data " + d.start);
     d.active = undefined;
     osc.stop()
 }
 
 
 function playStep() {
-    if (window.compostState.abort){
-        console.log("abort flag set");
-        for (const o of window.compostState.contentTypeOscs.values()){
-            try {
-                o.stop();
-            }
-            catch {}
-        }
-        return;
-    }
-    var now = window.compostState.current;
-    console.log("enter step: " + now);
-    window.compostState.current += stepSize;
-    var active = compostInputData.filter(function(e) { return now >= e.start && now <= e.start + e.duration; })
-    for (const d of compostInputData){
-        if (d.active !== undefined){
-            continue;
-        }
-        if (d.t === undefined){
-            console.log("type was undefined")
-            continue;
-        }
-        var osc = window.compostState.contentTypeOscs.get(d.t);
-        if (osc === undefined){
-            console.log("osc was undefined for type " + d.t)
-            continue;
-        }
-        try {
-            if (now >= d.start && now <= d.start + d.duration) {
-                d.active = true;
-
-                var osc = audioContext.createOscillator();
-                osc.connect(compostState.mainGainNode);
-                osc.type = "sine";
-                var degree = d.transferSize % 16 - 8;
-                osc.frequency.value = window.compostState.scaleDegrees(degree);
-                console.log("start osc " + d.t + " at " + now);
-                osc.start();
-                setTimeout(stopPlayingData.bind(d, osc), d.duration);
-            }
-        }
-        catch (e) {
-            console.log("error (probably benign) " + e);
-        }
-    }
-
-    if (now < window.compostState.loopAfter){
-        setTimeout(playStep, stepSize * 10);
-    }
-    else {
-        console.log("no more");
+    for (let d of compostInputData) {
+        var startTime = d.start % compostState.loopAfter;
+        console.log("scheduling data " + d.start + " in " + startTime + "ms");
+        setTimeout(() => {startPlayingData(d)}, d.start)
     }
 }
 window.compostState.playStep = playStep;
-window.compostState.stopAll = stopAll;
